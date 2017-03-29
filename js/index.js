fef3f1ce3bf93c1f5b61d3d73958ebaf1b5f70a3
ershubhamgarg <ershubhamgarg@yahoo.in>
2017-03-29 17:15:55 +0530

    acadview 2nd project

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('document').ready(function(){
+	$('.km1').on('click',function(){
+
+$('.tginfo1').toggleClass('tginfoo');
+	});
+
+	$('.km2').on('click',function(){
+$('.tginfo2').toggleClass('tginfoo');
+	});
+
+	$('.km3').on('click',function(){
+$('.tginfo3').toggleClass('tginfoo');
+	});
+});
